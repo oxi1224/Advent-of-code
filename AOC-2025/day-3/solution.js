3ab61079acac92c3f482e4a9f3e263ade33c149f
oxi1224 <oxi1224@o2.pl>
2025-12-04 16:34:49 +0100

    AOC-2025 day 3

---
+++ AOC-2025/day-3/solution.js
@@ -0,0 +1,58 @@
+/** @type {String[]} */
+const data = (await (Bun.file(Bun.argv[2])).text()).split("\n");
+
+function part_one() {
+  const joltages = [];
+  for (let i = 0; i < data.length; i++) {
+    const line = data[i];
+    const max = [0, 0]; // 0 => 1st switch, 1 => 2nd switch, 2 => 1st index
+    let max_idx = 0;
+    for (let j = 0; j < line.length - 2; j++) {
+      let n = parseInt(line[j]);
+      if (n > max[0]) {
+        max[0] = n;
+        max_idx = j;
+      }
+    }
+    for (let j = max_idx + 1; j < line.length; j++) {
+      let n = parseInt(line[j]);
+      if (n > max[1]) max[1] = n;
+    }
+    let joltage = 10 * max[0] + max[1];
+    joltages.push(joltage);
+  }
+
+  let s = 0
+  for (const j of joltages) {
+    s += j;
+  }
+  console.log(s);
+}
+
+const MAX_SWITCHES = 12;
+function part_two() {
+  const joltages = [];
+  for (let i = 0; i < data.length; i++) {
+    const line = data[i].trim();
+    const max = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let max_idx = 0;
+
+    for (let j = 0; j < MAX_SWITCHES; j++) {
+      for (let k = max_idx; k <= line.length - (MAX_SWITCHES - j); k++) {
+        let n = parseInt(line[k]);
+        if (n > max[j]) {
+          max[j] = n;
+          max_idx = k + 1;
+        }
+      }
+    }
+    let joltage = 0;
+    for (let j = 0; j < MAX_SWITCHES; j++) joltage += max[j] * 10 ** (MAX_SWITCHES - 1 - j);
+    joltages.push(joltage);
+  }
+
+  let s = 0
+  for (const j of joltages) s += j;
+  console.log(s);
+}
+part_two();
